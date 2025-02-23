63b83692d12b687487f5ad5f6e466fa1355a0a84
Cakala1 <74049579+Cakala1@users.noreply.github.com>
2025-02-23 20:13:08 +0100

    fixed imports

--- ChessEngine/main.c
+++ ChessEngine/main.c
@@ -1,16 +1,9 @@
 ﻿#include <stdio.h>
-#include <string.h>
-#include <Windows.h>
 #include "board.h"
 #include "attacks.h"
 #include "movegen.h"
-#include "globals.h"
-#include "randoms.h"
-#include "utils.h"
-#include "perft.h"
 #include "ui.h"
-#include "evaluate.h"
-#include "search.h"
+
 
 // ================================
 // ============= MAIN =============
@@ -24,15 +17,6 @@ int main() {
 	Board board;
 	reset_board(&board);
 	AttackTables* attacks = init_attack_tables();
-
-	/*int debug = 0;
-	if (debug) {
-		parse_FEN(&board, start_position);
-		print_board(&board);
-		search_position(&board, attacks, 4);
-	}
-	else uci_main(&board, attacks);*/
-
 	uci_main(&board, attacks);
 
 	return 0;
